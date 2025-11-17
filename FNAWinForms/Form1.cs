dff75317a0d2dee190721c1fd61a113fac3a1f6f
David Kutnar <cooty125@users.noreply.github.com>
2025-11-17 13:54:52 +0100

    Update Form1.cs

--- FNAWinForms/Form1.cs
+++ FNAWinForms/Form1.cs
@@ -7,9 +7,11 @@ namespace FNAWinForms
         {
             InitializeComponent();
 
-            fna = new FNA();
-            fna.Dock = DockStyle.Fill;
-            this.splitContainer1.Panel2.Controls.Add(fna);
+            this.fna = new FNA();
+            this.fna.FPSMax = 60;
+            this.fna.Dock = DockStyle.Fill;
+            this.splitContainer1.Panel2.Controls.Add(this.fna);
         }
     }
 }
+
