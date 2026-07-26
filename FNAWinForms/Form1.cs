c5fccb88d5f1ba4e90993d593923c886bb434d84
David Kutnar <cooty125@users.noreply.github.com>
2026-07-26 19:45:55 +0200

    Update Form1.cs

--- FNAWinForms/Form1.cs
+++ FNAWinForms/Form1.cs
@@ -9,7 +9,6 @@ namespace FNAWinForms
 
             this.fna = new FNA();
             this.fna.Dock = DockStyle.Fill;
-            this.fna.MultiSampleCount = 4;
             this.splitContainer1.Panel2.Controls.Add(this.fna);
         }
     }
